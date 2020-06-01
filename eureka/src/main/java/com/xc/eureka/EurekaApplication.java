b3c2fcba71925745b4babe8d814a9ed7fb615395
Shaper <277090250@qq.com>
2020-06-01 17:56:06 +0800

    pom修改

--- eureka/src/main/java/com/xc/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/xc/eureka/EurekaApplication.java
@@ -1,9 +1,12 @@
 package com.xc.eureka;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class EurekaApplication {
 
-	/*public static void main(String[] args) {
-		EurekaApplication.run(EurekaApplication.class, args);
-	}*/
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
 }
