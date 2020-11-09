5aa9487aca5eb7d53e4f2671efcf2a9f23dc0fbf
shaper <277090250@qq.com>
2020-11-09 16:44:24 +0800

    通过feign接口，调用服务。 consumer-feign引入并调用service模块的接口 service模块引入feign，且将security代码封装到service模块中。

---
+++ service/src/main/java/com/xc/service/IProductClientService.java
@@ -0,0 +1,22 @@
+package com.xc.service;
+
+import com.xc.config.FeignClientConfig;
+import com.xc.pojo.Product;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@FeignClient(name = "RELATION-PRO",configuration = FeignClientConfig.class)
+public interface IProductClientService {
+    @RequestMapping("/product/get/{id}")
+    public Product getProduct(@PathVariable("id") long id);
+
+    @RequestMapping("/product/list")
+    public List<Product> listProduct() ;
+
+    @RequestMapping("/product/add")
+    public boolean addPorduct(Product product) ;
+
+}
