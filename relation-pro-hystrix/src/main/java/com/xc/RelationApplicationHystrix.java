28fa06db56589e92ecd0e53460a25d9ad40da83c
shaper <277090250@qq.com>
2020-11-10 15:44:55 +0800

    hystrix - 服务降级

---
+++ relation-pro-hystrix/src/main/java/com/xc/RelationApplicationHystrix.java
@@ -0,0 +1,19 @@
+package com.xc;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@MapperScan("com.xc.mapper")
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableCircuitBreaker	//熔断机制
+public class RelationApplicationHystrix {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelationApplicationHystrix.class,args);
+    }
+}
