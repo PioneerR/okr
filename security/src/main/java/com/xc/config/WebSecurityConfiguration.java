2e855de78371d86d08776fe9d2f2623c1bd6dbc5
shaper <277090250@qq.com>
2020-11-11 00:08:50 +0800

    一些url，不需要权限认证

--- security/src/main/java/com/xc/config/WebSecurityConfiguration.java
+++ security/src/main/java/com/xc/config/WebSecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.xc.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -31,6 +32,15 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
     }
 
+    /**
+     * 以下请求，不需要做权限控制
+     *
+     * @author shaper 2020/11/11 0:04
+     */
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/actuator/hystrix.stream", "/turbine.stream") ;
+	}
 
 
 }
