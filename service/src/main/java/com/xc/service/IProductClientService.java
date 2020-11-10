d5a85019e2ad2ff5c5725a58b2c2776b1be9e0d9
shaper <277090250@qq.com>
2020-11-10 16:14:49 +0800

    熔断机制代码

--- service/src/main/java/com/xc/service/IProductClientService.java
+++ service/src/main/java/com/xc/service/IProductClientService.java
@@ -2,13 +2,15 @@ package com.xc.service;
 
 import com.xc.config.FeignClientConfig;
 import com.xc.pojo.Product;
+import com.xc.service.fallback.IProductClientServiceFallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@FeignClient(name = "RELATION-PRO",configuration = FeignClientConfig.class)
+@FeignClient(name = "RELATION-PRO",configuration = FeignClientConfig.class,
+				fallbackFactory = IProductClientServiceFallbackFactory.class)
 public interface IProductClientService {
     @RequestMapping("/product/get/{id}")
     public Product getProduct(@PathVariable("id") long id);
